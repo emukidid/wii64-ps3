88e78ce26e85976049305912ea81446ec3cccc96
emukidid <emukidid@gmail.com>
2011-03-16 23:28:04 +0000

    Use faster invalid code array

--- r4300/Invalid_Code.c
+++ r4300/Invalid_Code.c
@@ -3,23 +3,8 @@
  */
 
 #include "Invalid_Code.h"
-#ifndef HW_RVL  //GC use bit version
 
-static unsigned char invalid_code[0x100000/8];
-
-int inline invalid_code_get(int block_num){
-	return invalid_code[block_num>>3] & (1<<(block_num&0x7));
-}
-
-void inline invalid_code_set(int block_num, int value){
-	if(value) invalid_code[block_num>>3] |=  (1<<(block_num&0x7));
-	else      invalid_code[block_num>>3] &= ~(1<<(block_num&0x7));
-}
-
-#else //Wii MEM2 1MB char array version
-#include "../gc_memory/MEM2.h"
-
-static unsigned char *invalid_code = (unsigned char *)(INVCODE_LO);
+static unsigned char invalid_code[0x100000];
 
 int inline invalid_code_get(int block_num){
 	return invalid_code[block_num];
@@ -28,6 +13,4 @@ int inline invalid_code_get(int block_num){
 void inline invalid_code_set(int block_num, int value){
 	invalid_code[block_num] = value;
 }
-
-#endif
 
